b671f8407351b22b0768885d03ddf6fed2ab50bf
JD <114588726+mhVxVNMwXkm9s4Ze9VASei@users.noreply.github.com>
2023-08-18 21:00:09 -0500

    Learned about refs.
    Pretty neat. :0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const apiKey = "873cb116";
@@ -373,13 +373,31 @@ function NumResults({ movies }) {
 }
 
 function SearchBar({ query, setQuery }) {
+	const inputElement = useRef(null);
+
+	useEffect(() => {
+		function callback(event) {
+			if (document.activeElement === inputElement.current) return;
+
+			if (event.code === "Enter") {
+				inputElement.current.focus();
+				setQuery("");
+			}
+		}
+
+		document.addEventListener("keydown", callback);
+
+		return () => document.addEventListener("keydown", callback);
+	}, [setQuery]);
+
 	return (
 		<input
 			className="search"
 			type="text"
-			placeholder="Search movies..."
-			value={query}
 			onChange={(e) => setQuery(e.target.value)}
+			placeholder="Search movies..."
+			ref={inputElement}
+			value={query}
 		/>
 	);
 }
